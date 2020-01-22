c4fbe41def231455aaf07ac42878491e94ae1ffa
Kate W <magpiedin@gmail.com>
2020-01-22 16:10:53 -0600

    include scientificName

--- IPTrr_PreDev.R
+++ IPTrr_PreDev.R
@@ -13,44 +13,58 @@ library(tidyr)
 
 relat_raw <- read_csv("data01raw/relationships/Group1.csv")
 
-# Group.DarGlobalUniqueID      = resourceID  (= occurrenceID)
-# Group.relatedResourceID      = relatedResourceID
-# Group.RelRelationship_tab    = relationshipOfResource
-# Group.RelNotes               = relationshipRemarks
-# "[to be parsed]"             = relationshipAccordingTo
-# "[to be parsed]"             = relationshipEstablishedDate
-# "[to be parsed]"             = scientificName
+# Group.DarGlobalUniqueID                     = resourceID  (= occurrenceID)
+# Group.relatedResourceID                     = relatedResourceID
+# Group.RelRelationship_tab                   = relationshipOfResource
+# Group.DarScientificName/[to be parsed]"     = scientificName
+# Group.RelNotes                              = relationshipRemarks
+# "[to be parsed]"                            = relationshipAccordingTo
+# "[to be parsed]"                            = relationshipEstablishedDate
+
 
 relat <- data.frame("resourceID" = relat_raw$DarGlobalUniqueIdentifier,
                     "relatedResourceID" = relat_raw$relatedResourceID,
                     "relationshipOfResource" = relat_raw$RelRelationship,
-                    "relationshipRemarks" = relat_raw$RelNotes,
+                    "scientificName" = relat_raw$DarScientificName, 
+                    "RelNotes" = relat_raw$RelNotes,
                     stringsAsFactors = FALSE)
 
 
 #  Parse RelNotes
 #
 #   RelNotes values from EMu need to be formatted like this:
-#    “Count: [value] | ObjectURI: [OBJECT GUID or URI if not FMNH catalogue record] | RecordedBy: [Recorder-1 irn, Recorder-2 irn] | TaxonIRN: [irn] | TaxonSummary: [Summary] | Notes: [Text from notes”
-#
+#    “Count: [value] | ObjectURI: [OBJECT GUID or URI if not FMNH catalogue record] | RecordedByIRN: [Recorder-1 irn, Recorder-2 irn] | RecordedBySummary: [Recorder-1 name, Recorder-2 name] | TaxonIRN: [irn] | TaxonSummary: [Summary] | Notes: [Text from notes]”
 
-relat <- separate(relat, col = "relationshipRemarks",
-                  into = c("Count", "relatedResourceID_2", "relationshipAccordingTo",
-                           "TaxonIRN", "scientificName", "relationshipRemarks"),
-                  sep = "\\|", remove = TRUE, convert = FALSE,
-                  extra = "warn", fill = "warn")
+relat$Count <- gsub("Count:\\s*|\\s*\\|\\s*ObjURI:.*", "", relat$RelNotes)
+relat$relatedResourceID_2 <- gsub(".*ObjURI:\\s*|\\s*\\|\\s*RecordedByIRN.*", "", relat$RelNotes)
+relat$RecordedByIRN <- gsub(".*RecordedByIRN:\\s*|\\s*\\|\\s*RecordedBySummary.*", "", relat$RelNotes)
+relat$relationshipAccordingTo <- gsub(".*RecordedBySummary:\\s*|\\s*\\|\\s*TaxonIRN.*", "", relat$RelNotes)
+relat$TaxonIRN <- gsub(".*TaxonIRN:\\s*|\\s*\\|\\s*TaxonSummary.*", "", relat$RelNotes)
+relat$scientificName_2 <- gsub(".*TaxonSummary:\\s*|\\s*\\|\\s*Notes.*", "", relat$RelNotes)
+relat$relationshipRemarks <- gsub(".*\\|\\s*Notes:\\s*", "", relat$RelNotes)
+
+
+# # Separate seems simpler but more fragile if RelNotes value doesn't strictly follow format
+# relat <- separate(relat, col = "relationshipRemarks",
+#                   into = c("Count", "relatedResourceID_2", 
+#                            "RecordedByIRN", "relationshipAccordingTo",
+#                            "TaxonIRN", "scientificName_2", "relationshipRemarks"),
+#                   sep = "\\|", remove = TRUE, convert = FALSE,
+#                   extra = "warn", fill = "warn")
+#
+# # Cleanup parsed values
+# relat$Count <- gsub("Count:\\s*", "", relat$Count)
+# relat$relatedResourceID_2 <- gsub("ObjURI:\\s*", "", relat$relatedResourceID_2)
+# relat$relationshipAccordingTo <- gsub("RecordedBy:\\s*", "", relat$relationshipAccordingTo)
+# relat$TaxonIRN <- gsub("TaxonIRN:\\s*", "", relat$TaxonIRN)
+# relat$scientificName_2 <- gsub("TaxonSummary:\\s*", "", relat$scientificName_2)
+# relat$relationshipRemarks <- gsub("Notes:\\s*", "", relat$relationshipRemarks)
+
 
 relat <- as.data.frame(sapply(relat, trimws, simplify = FALSE),
                        stringsAsFactors = F)
 
 
-# Cleanup parsed values
-relat$Count <- gsub("Count:\\s*", "", relat$Count)
-relat$relatedResourceID_2 <- gsub("ObjURI:\\s*", "", relat$relatedResourceID_2)
-relat$relationshipAccordingTo <- gsub("RecordedBy:\\s*", "", relat$relationshipAccordingTo)
-relat$TaxonIRN <- gsub("TaxonIRN:\\s*", "", relat$TaxonIRN)
-relat$scientificName <- gsub("TaxonSummary:\\s*", "", relat$scientificName)
-relat$relationshipRemarks <- gsub("Notes:\\s*", "", relat$relationshipRemarks)
 
 relat <- as.data.frame(sapply(relat, gsub, pattern = "NULL", replacement = "",
                               simplify = FALSE),
@@ -59,6 +73,7 @@ relat <- as.data.frame(sapply(relat, gsub, pattern = "NULL", replacement = "",
 
 # Merge fields mapped to multiple pre-dev fields
 relat$relatedResourceID[is.na(relat$relatedResourceID)==T] <- relat$relatedResourceID_2[is.na(relat$relatedResourceID)==T]
+relat$scientificName[is.na(relat$scientificName)==T] <- relat$scientificName_2[is.na(relat$scientificName)==T]
 
 
 # Add placeholders for missing fields
@@ -83,6 +98,7 @@ write.csv(relat_out,
           file = paste0("data02output/relation/relation_",
                         gsub("-|\\s+|:", "", Sys.time()),
                         ".csv"),
+          row.names = FALSE,
           quote = TRUE,
           na = "")
 
